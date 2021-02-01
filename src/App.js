514832a4ff3f591d548bc1418c66d7244b981fdc
Tom Duffy <thomas.edward.duffy@gmail.com>
2021-02-01 11:45:54 -0800

    installed and initialized dependencies

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import './App.css';
+
+const App = () => {
+    return (
+        <ChatEngine 
+            height="100vh"
+            projectID="6d7bad61-3577-4fe1-9534-e693cbfbe96d"
+            userName="tduffy"
+            userSecret="123123"
+        />
+    );
+}
+
+export default App;
